ba60957821dc0261d78e41308095f3ef60d6067f
Weigaa <38322329+Weigaa@users.noreply.github.com>
2021-04-01 16:35:25 +0800

    Add files via upload

---
+++ GPU版本/GPUPANDAtrain_dist_VGG16_cifarwithmomentum.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+import os
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import time
+import random
+
+from math import ceil
+from random import Random
+from torch.multiprocessing import Process
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+device = torch.device('cuda:0')
+torch.backends.cudnn.benchmark = True
+
+
+class Partition(object):
+    """ Dataset-like object, but only access a subset of it. """
+
+    def __init__(self, data, index):
+        self.data = data
+        self.index = index
+
+    def __len__(self):
+        return len(self.index)
+
+    def __getitem__(self, index):
+        data_idx = self.index[index]
+        return self.data[data_idx]
+
+
+class DataPartitioner(object):
+    """ Partitions a dataset into different chuncks. """
+    def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
+        self.data = data
+        self.partitions = []
+        rng = Random()
+        rng.seed(seed)
+        data_len = len(data)
+        indexes = [x for x in range(0, data_len)]
+        rng.shuffle(indexes)
+
+        for frac in sizes:
+            part_len = int(frac * data_len)
+            self.partitions.append(indexes[0:part_len])
+            indexes = indexes[part_len:]
+
+    def use(self, partition):
+        return Partition(self.data, self.partitions[partition])
+
+
+class Net(nn.Module):
+    """ vgg16 Network architecture. """
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, 3, padding=1)
+        self.conv2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu1 = nn.ReLU()
+
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv4 = nn.Conv2d(128, 128, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.relu2 = nn.ReLU()
+
+        self.conv5 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=1)
+        self.conv7 = nn.Conv2d(128, 128, 1, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.ReLU()
+
+        self.conv8 = nn.Conv2d(128, 256, 3, padding=1)
+        self.conv9 = nn.Conv2d(256, 256, 3, padding=1)
+        self.conv10 = nn.Conv2d(256, 256, 1, padding=1)
+        self.pool4 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.relu4 = nn.ReLU()
+
+        self.conv11 = nn.Conv2d(256, 512, 3, padding=1)
+        self.conv12 = nn.Conv2d(512, 512, 3, padding=1)
+        self.conv13 = nn.Conv2d(512, 512, 1, padding=1)
+        self.pool5 = nn.MaxPool2d(2, 2, padding=1)
+        self.bn5 = nn.BatchNorm2d(512)
+        self.relu5 = nn.ReLU()
+
+        self.fc14 = nn.Linear(512 * 4 * 4, 1024)
+        self.drop1 = nn.Dropout2d()
+        self.fc15 = nn.Linear(1024, 1024)
+        self.drop2 = nn.Dropout2d()
+        self.fc16 = nn.Linear(1024, 10)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.pool1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.pool2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+
+        x = self.conv5(x)
+        x = self.conv6(x)
+        x = self.conv7(x)
+        x = self.pool3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+
+        x = self.conv8(x)
+        x = self.conv9(x)
+        x = self.conv10(x)
+        x = self.pool4(x)
+        x = self.bn4(x)
+        x = self.relu4(x)
+
+        x = self.conv11(x)
+        x = self.conv12(x)
+        x = self.conv13(x)
+        x = self.pool5(x)
+        x = self.bn5(x)
+        x = self.relu5(x)
+
+        x = x.view(-1, 512 * 4 * 4)
+        x = F.relu(self.fc14(x))
+        x = self.drop1(x)
+        x = F.relu(self.fc15(x))
+        x = self.drop2(x)
+        x = self.fc16(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+    # #和非分布式一致的网络
+    # def __init__(self):
+    #     super(Net, self).__init__()
+    #     self.conv1 = nn.Conv2d(1, 32, 3, 1)
+    #     self.conv2 = nn.Conv2d(32, 64, 3, 1)
+    #     self.dropout1 = nn.Dropout2d(0.25)
+    #     self.dropout2 = nn.Dropout2d(0.5)
+    #     self.fc1 = nn.Linear(9216, 128)
+    #     self.fc2 = nn.Linear(128, 10)
+    #
+    # def forward(self, x):
+    #     x = self.conv1(x)
+    #     x = F.relu(x)
+    #     x = self.conv2(x)
+    #     x = F.relu(x)
+    #     x = F.max_pool2d(x, 2)
+    #     x = self.dropout1(x)
+    #     x = torch.flatten(x, 1)
+    #     x = self.fc1(x)
+    #     x = F.relu(x)
+    #     x = self.dropout2(x)
+    #     x = self.fc2(x)
+    #     output = F.log_softmax(x, dim=1)
+    #     return output
+
+
+def partition_dataset():
+    """ Partitioning CIFAR10 """
+    dataset = datasets.CIFAR10(
+        './data',
+        train=True,
+        download=True,
+        transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomGrayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,0.5,0.5 ), (0.5,0.5,0.5 ))
+        ]))
+    size = dist.get_world_size()
+    bsz = 128 / float(size)
+    partition_sizes = [1.0 / size for _ in range(size)]
+    partition = DataPartitioner(dataset, partition_sizes)
+    partition = partition.use(dist.get_rank())
+    train_set = torch.utils.data.DataLoader(
+        partition, batch_size=int(bsz), shuffle=True)
+    return train_set, bsz
+
+
+def average_gradients(model):
+    """ Gradient averaging. """
+    size = float(dist.get_world_size())
+    for param in model.parameters():
+        # dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM, group=0)
+        dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
+        param.grad.data /= size
+
+def getbesthyperparameter(tensorlist,tensor):
+    #share loss and tensor
+    dist.all_gather(tensorlist,tensor)
+
+def run(rank, size):
+    """ Distributed Synchronous SGD Example """
+    torch.manual_seed(1234)
+    train_set, bsz = partition_dataset()
+    model = Net().to(device)
+    tensor_list = [torch.zeros(3, dtype=torch.float).to(device) for _ in range(dist.get_world_size())]
+    mytensor_list = [[0,0,0] for _ in range(dist.get_world_size())]
+#    model = model.cuda(rank)
+    # 使用PBT优化选择不同的lr和momentum
+    # optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    #使用PBT优化选择不同的lr和momentum
+    nativelr = random.uniform(0.01,0.1)
+    naivemomentum = random.uniform(0.5,0.9)
+    lr = nativelr
+    optimizer = optim.SGD(model.parameters(), lr=nativelr, momentum=naivemomentum)
+    num_batches = ceil(len(train_set.dataset) / float(bsz))
+    totalbegin = time.time()
+    for epoch in range(10):
+        epoch_loss = 0.0
+        begin = time.time()
+        for data, target in train_set:
+            data, target = Variable(data), Variable(target)
+#            data, target = Variable(data.cuda(rank)), Variable(target.cuda(rank))
+            optimizer.zero_grad()
+            data = data.to(device)
+            target = target.to(device)
+            output = model(data)
+            loss = F.nll_loss(output, target).to(device)
+            epoch_loss += loss.item()
+            loss.backward()
+            average_gradients(model)
+            optimizer.step()
+        end = time.time()
+        spendtime = (end-begin)/60
+        print('Rank ',
+              dist.get_rank(), ', epoch ', epoch, ': ',
+              epoch_loss / num_batches,', spend time: ',spendtime)
+        # 每个epoch结束后修改lr和momentum
+        realoss = epoch_loss / num_batches
+        sharetensor = torch.tensor([realoss,lr,naivemomentum]).to(device)
+        getbesthyperparameter(tensor_list,sharetensor)
+        for i in range(len(tensor_list)):
+            mytensor_list[i] = tensor_list[i].tolist()
+        print(mytensor_list)
+        bestrank = mytensor_list.index(min(mytensor_list))
+        if dist.get_rank() != bestrank:
+            lr = mytensor_list[bestrank][1] * random.uniform(0.8,1.2)
+            naivemomentum = mytensor_list[bestrank][2] * random.uniform(0.8,1.2)
+            if (naivemomentum > 1):
+                naivemomentum = mytensor_list[bestrank][2] * random.uniform(0.8,0.99)
+        optimizer = optim.SGD(model.parameters(), lr=lr, momentum=naivemomentum)
+
+
+    totalend = time.time()
+    print("total realrun time: ", (totalend - totalbegin) / 60)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.CIFAR10(
+            './data',
+            train=False,
+            transform=transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    transforms.Normalize(
+                        (0.5,0.5,0.5
+                         ),
+                        (0.5,0.5,0.5
+                         ))])),
+        batch_size=32,
+        shuffle=True,
+    )
+    get_accuracy(test_loader, model)
+
+def get_accuracy(test_loader, model):
+    model.eval()
+    correct_sum = 0
+    # Use GPU to evaluate if possible
+    # device = torch.device("cpu")
+    with torch.no_grad():
+        for i, (data, target) in enumerate(test_loader):
+            data = data.to(device)
+            out = model(data).to(device)
+            pred = out.argmax(dim=1, keepdim=True)
+            pred, target = pred.to(device), target.to(device)
+            correct = pred.eq(target.view_as(pred)).sum().item()
+            correct_sum = correct_sum + correct
+
+    print(f"Accuracy {correct_sum / len(test_loader.dataset)}")
+
+
+def init_processes(rank, size, fn, backend='gloo'):
+    """ Initialize the distributed environment. """
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '29500'
+    dist.init_process_group(backend, rank=rank, world_size=size)
+    fn(rank, size)
+
+
+if __name__ == "__main__":
+    size = 4
+    processes = []
+    print("execution beginning...")
+    print("all process BEGINtime is : ", time.strftime('%Y-%m-%d %H:%M:%S'))
+    for rank in range(size):
+        p = Process(target=init_processes, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+    print("all process ENDtime is : ", time.strftime('%Y-%m-%d %H:%M:%S'))
